7ad5eb643fef8334d9d438aa57d6dff89d90aed7
Nuryon <dutaillykevin.pro@gmail.com>
2024-04-26 17:12:34 +0200

    final

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,64 @@
 const slides = [
-    {
-        "image":"slide1.jpg",
-        "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-    },
-    {
-        "image":"slide2.jpg",
-        "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-    },
-    {
-        "image":"slide3.jpg",
-        "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-    },
-    {
-        "image":"slide4.png",
-        "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-    }
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
 ];
 
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dots = document.querySelector(".dots");
+const ImageSlide = document.querySelector(".banner-img");
+const TextSlider = document.querySelector("#banner p");
 
-  
-    const arrowLeft = document.querySelector(".arrow_left");
-    const arrowRight = document.querySelector(".arrow_right");
-    const dotsContainer = document.querySelector(".dots");
-   const ImageSlide = document.querySelector(".banner-img")
-   const TextSlider = document.querySelector(".slider-text")
-	let dots = [];
+let index = 0;
 
-   
+const generateBulletPoints = () => {
+  dots.innerHTML = "";
+  for (let i = 0; i < slides.length; i++) {
+    let dot = document.createElement("p");
+    if (i === index) {
+      dot.className = "dot dot_selected";
+    } else {
+      dot.className = "dot";
+    }
+    dots.appendChild(dot);
+  }
+};
 
-	for ( let i=0 ; i < slides.length; i ++ ){
-		let dot = document.createElement("p");
-		dotsContainer.appendChild(dot);
-		dot.classList.add('dot');
-		dots.push(dot);
-	}
+generateBulletPoints();
+const changeContent = () => {
+  ImageSlide.src = "./assets/images/slideshow/" + slides[index].image;
+  TextSlider.innerHTML = slides[index].tagLine;
+  generateBulletPoints();
+};
 
-	dots[0].classList.add ('dot_selected');
+arrowLeft.addEventListener("click", () => {
+  index = index - 1;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  changeContent();
+});
 
-	 arrowLeft.addEventListener("click", () => {
-        console.log("Vous avez bien cliqué sur la flèche gauche.");
-    });
+arrowRight.addEventListener("click", () => {
+  index = index + 1;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
 
-	arrowRight.addEventListener("click", () => {
-        console.log("Vous avez bien cliqué sur la flèche droite.");
-    });
-
+  changeContent();
+});
